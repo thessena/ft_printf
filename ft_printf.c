efa0ce2261a87984169d7e46beafe6017122fc94
thessena <timhessenauer@gmail.com>
2024-11-01 11:01:59 +0100

    implemented ft_putnbr in the function

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:32:32 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/01 10:15:26 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/01 10:57:00 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,33 @@ int	ft_putchar(char c)
 	return (1);
 }
 
+int	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		write(1, "-2", 2);
+		n = 147483648;
+	}
+	if (n < 0)
+	{
+		ft_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+		ft_putnbr(n / 10);
+	ft_putchar((n %= 10) + 48);
+	return (1);
+}
+
 int	check_type(char type, va_list args)
 {
 	int	len;
 
 	len = 0;
 	if (type == 'c')
-	{
 		len += ft_putchar(va_arg(args, int));
-	}
+	else if (type == 'i')
+		len += ft_putnbr(va_arg(args, int));
 	return (len);
 }
 
--- test_ft_printf.c
+++ test_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:23:37 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/01 10:32:18 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/01 10:58:34 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,9 @@ int	ft_printf(const char *text, ...);
 
 int	main(void)
 {
-	int	len;
-
-	len = 0;
-	len += ft_printf("Hallo Welt\n");
-	printf("Zeichen: %i\n", len);
-	len = 0;
-	len += ft_printf("Char: %c\n", 'A');
-	printf("Zeichen: %i\n", len);
-	// ft_printf("Zahl: %i\n", 42);
+	ft_printf("Hallo Welt\n");
+	ft_printf("Char: %c\n", 'A');
+	ft_printf("Zahl: %i\n", 42);
 	// ft_printf("String: %s\n", "Test");
 	// ft_printf("Hexadezimal: %x\n", 255);
 	// ft_printf("Große Zahl: %d\n", INT_MAX);
