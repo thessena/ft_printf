1289da411e6b9a38f0017a0beab34aa117585278
thessena <timhessenauer@gmail.com>
2024-11-01 10:09:57 +0100

    updated printf, started checking for types and giving action for type character

--- ft_printf.c
+++ ft_printf.c
@@ -6,32 +6,48 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:32:32 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/31 11:27:35 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/01 10:06:33 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	check_type(char type, va_list args)
+{
+	int	len;
+
+	len = 0;
+	if (type == 'c')
+	{
+		len += ft_putchar(va_arg(args, int));
+	}
+	return (len);
+}
+
 int	ft_printf(const char *text, ...)
 {
 	va_list	args;
-	int		count;
+	int		len;
 
 	va_start(args, text);
-	count = 0;
+	len = 0;
 	while (*text)
 	{
 		if (*text == '%')
 		{
 			text++;
+			len += check_type(*text, args);
 		}
 		else
-		{
-			write(1, text, 1);
-			count++;
-		}
+			len += ft_putchar(*text);
 		text++;
 	}
 	va_end(args);
-	return (count);
+	return (len);
 }
--- test_ft_printf.c
+++ test_ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 10:23:37 by thessena          #+#    #+#             */
-/*   Updated: 2024/10/31 11:27:28 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/01 10:04:20 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,16 @@ void	test_ft_printf(const char *text, ...)
 	int		printf_result;
 	int		ft_printf_result;
 
-	// printf("Standard printf:\n");
-	// va_start(args1, text);
-	// printf_result = vprintf(text, args1);
-	// va_end(args1);
-	// printf("\nAnzahl Zeichen (printf): %d\n", printf_result);
+	printf("\nStandard printf:\n");
+	va_start(args1, text);
+	printf_result = vprintf(text, args1);
+	va_end(args1);
+	printf("Anzahl Zeichen (printf): %d\n", printf_result);
 	printf("\nEigene ft_printf:\n");
 	va_start(args2, text);
 	ft_printf_result = ft_printf(text, args2);
 	va_end(args2);
-	printf("Anzahl Zeichen (ft_printf): %d\n", ft_printf_result);
+	printf("Anzahl Zeichen (ft_printf): %d\n\n", ft_printf_result);
 	if (printf_result == ft_printf_result)
 		printf("Erfolg: Funktionen geben gleiche Anzahl von Zeichen aus.\n");
 	else
@@ -42,7 +42,7 @@ void	test_ft_printf(const char *text, ...)
 int	main(void)
 {
 	test_ft_printf("Hallo Welt\n");
-	test_ft_printf("Zahl: %d\n", 42);
+	// test_ft_printf("Zahl: %d\n", 42);
 	// test_ft_printf("String: %s\n", "Test");
 	// test_ft_printf("Char: %c\n", 'A');
 	// test_ft_printf("Hexadezimal: %x\n", 255);
