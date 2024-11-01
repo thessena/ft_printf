cdd7e799f78fd20a3e38319860ac74c737bd5b1b
thessena <timhessenauer@gmail.com>
2024-11-01 11:51:58 +0100

    fixed problem in integer case, paco is ok now for %i

--- ft_printf.c
+++ ft_printf.c
@@ -6,19 +6,19 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 09:32:32 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/01 11:27:15 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/01 11:50:51 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_putchar(char c)
+int	ft_printchar(char c)
 {
 	write(1, &c, 1);
 	return (1);
 }
 
-int	ft_putstr(char *s)
+int	ft_printstr(char *s)
 {
 	int	i;
 
@@ -31,21 +31,31 @@ int	ft_putstr(char *s)
 	return (i);
 }
 
-int	ft_putnbr(int n)
+int	ft_printnbr(int n)
 {
+	int	i;
+
+	i = 0;
 	if (n == -2147483648)
 	{
 		write(1, "-2", 2);
 		n = 147483648;
+		i += 2;
 	}
 	if (n < 0)
 	{
-		ft_putchar('-');
+		i += ft_printchar('-');
 		n = -n;
 	}
 	if (n >= 10)
-		ft_putnbr(n / 10);
-	ft_putchar((n %= 10) + 48);
+		i += ft_printnbr(n / 10);
+	i += ft_printchar((n %= 10) + 48);
+	return (i);
+}
+
+int	ft_printperc(void)
+{
+	write(1, "%", 1);
 	return (1);
 }
 
@@ -55,11 +65,13 @@ int	check_type(char type, va_list args)
 
 	len = 0;
 	if (type == 'c')
-		len += ft_putchar(va_arg(args, int));
+		len += ft_printchar(va_arg(args, int));
 	else if (type == 's')
-		len += ft_putstr(va_arg(args, char *));
+		len += ft_printstr(va_arg(args, char *));
 	else if (type == 'i')
-		len += ft_putnbr(va_arg(args, int));
+		len += ft_printnbr(va_arg(args, int));
+	else if (type == '%')
+		len += ft_printperc();
 	return (len);
 }
 
@@ -78,7 +90,7 @@ int	ft_printf(const char *text, ...)
 			len += check_type(*text, args);
 		}
 		else
-			len += ft_putchar(*text);
+			len += ft_printchar(*text);
 		text++;
 	}
 	va_end(args);
