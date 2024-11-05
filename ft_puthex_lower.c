2c239395bc3243753e7a75b8f7658c62f2375c7a
thessena <timhessenauer@gmail.com>
2024-11-05 17:03:39 +0100

    corrected puthex_lower for paco -s

--- ft_puthex_lower.c
+++ ft_puthex_lower.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 13:46:00 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/05 15:04:29 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:02:41 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,13 @@ int	ft_puthex_lower(unsigned long n)
 	count = 0;
 	base = "0123456789abcdef";
 	if (n >= 16)
+	{
 		count += ft_puthex_lower(n / 16);
+		if (count == -1)
+			return (-1);
+	}
 	count += ft_putchar(base[n % 16]);
+	if (count == -1)
+		return (-1);
 	return (count);
 }
