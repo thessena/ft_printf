00c8c0dec190a56a8dd852661cfa2144d6da34b5
thessena <timhessenauer@gmail.com>
2024-11-05 17:46:29 +0100

    corrected putnbr_unsigned function

--- ft_putnbr_unsigned.c
+++ ft_putnbr_unsigned.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 13:39:38 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/05 13:42:34 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:45:48 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,18 @@
 int	ft_putnbr_unsigned(unsigned int n)
 {
 	int	i;
+	int	count;
 
 	i = 0;
+	count = 0;
 	if (n >= 10)
-		i += ft_putnbr_unsigned(n / 10);
-	i += ft_putchar((n % 10) + '0');
-	return (i);
+	{
+		count += ft_putnbr_unsigned(n / 10);
+		if (count == -1)
+			return (-1);
+		i += count;
+	}
+	if (ft_putchar((n % 10) + '0') == -1)
+		return (-1);
+	return (i + 1);
 }
