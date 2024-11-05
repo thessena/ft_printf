683a7b4b1a6e4b681477a35de22c49af8d245d60
thessena <timhessenauer@gmail.com>
2024-11-05 17:05:13 +0100

    corrected puthex_upper for paco -s return -1 case

--- ft_puthex_upper.c
+++ ft_puthex_upper.c
@@ -6,7 +6,7 @@
 /*   By: thessena <thessena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/05 14:44:37 by thessena          #+#    #+#             */
-/*   Updated: 2024/11/05 15:04:35 by thessena         ###   ########.fr       */
+/*   Updated: 2024/11/05 17:04:37 by thessena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,13 @@ int	ft_puthex_upper(unsigned long n)
 	count = 0;
 	base = "0123456789ABCDEF";
 	if (n >= 16)
+	{
 		count += ft_puthex_upper(n / 16);
+		if (count == -1)
+			return (-1);
+	}
 	count += ft_putchar(base[n % 16]);
+	if (count == -1)
+		return (-1);
 	return (count);
 }
